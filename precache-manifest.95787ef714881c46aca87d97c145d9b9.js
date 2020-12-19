2f651bdaf31a035155dcd5a3a9fb2cc91341fffd
ArktheLass <arkpeggs@gmail.com>
2020-12-19 11:10:52 -0600

    Updates

---
+++ precache-manifest.95787ef714881c46aca87d97c145d9b9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809c9f90ccaa7b66394adc4e132ef56b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e92b2c9345d2101dbde8",
+    "url": "/Portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7f8f8ac6d50f4f030e1c",
+    "url": "/Portfolio/static/css/main.2e3f1d4b.chunk.css"
+  },
+  {
+    "revision": "e92b2c9345d2101dbde8",
+    "url": "/Portfolio/static/js/2.51e63379.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Portfolio/static/js/2.51e63379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8f8ac6d50f4f030e1c",
+    "url": "/Portfolio/static/js/main.8f289e1c.chunk.js"
+  },
+  {
+    "revision": "16d288d4cf7d55bff3a4",
+    "url": "/Portfolio/static/js/runtime-main.b16c2259.js"
+  },
+  {
+    "revision": "9d30bd3724e15b6d69ef4f9036615de9",
+    "url": "/Portfolio/static/media/Note-taker.9d30bd37.PNG"
+  },
+  {
+    "revision": "0469a6d8dd90c7e3d83d49f5651f636b",
+    "url": "/Portfolio/static/media/WWCLOGO.0469a6d8.PNG"
+  },
+  {
+    "revision": "420171a20e38d849c01a2246cc612ee8",
+    "url": "/Portfolio/static/media/fitness-tracker.420171a2.PNG"
+  },
+  {
+    "revision": "e42f62570497a3d0774fd3321db99d10",
+    "url": "/Portfolio/static/media/flowers.e42f6257.jpg"
+  },
+  {
+    "revision": "e27f27130261f2375d67e95a8f12917f",
+    "url": "/Portfolio/static/media/ticket-tracker-logo.e27f2713.PNG"
+  },
+  {
+    "revision": "b4a84f0e5b170c7048b5c6894255c670",
+    "url": "/Portfolio/static/media/weather.b4a84f0e.PNG"
+  }
+]);
